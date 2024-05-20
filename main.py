f64d1109ddfeea239d03937a656bf14b20923fe7
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-20 12:57:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from object_renderer import *
 class Game:
     def __init__(self):
         pg.init()
+        pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
