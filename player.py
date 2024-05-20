7deab16476abb57f2d6cf50f92cb27b2de3ede21
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-20 12:52:35 -0300

    Update player.py

--- player.py
+++ player.py
@@ -55,9 +55,9 @@ class Player:
 
     #
     def draw(self):
-        #pg.draw.line(self.game.screen, 'green', (self.x * 100, self.y *100),
-        #             (self.x * 100 + WIDTH * math.cos(self.angle),
-        #              self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        pg.draw.line(self.game.screen, 'green', (self.x * 100, self.y *100),
+                     (self.x * 100 + WIDTH * math.cos(self.angle),
+                      self.y * 100 + WIDTH * math.sin(self.angle)), 2)
         pg.draw.circle(self.game.screen, 'red', (self.x * 100, self.y * 100), 15)
         
     #
