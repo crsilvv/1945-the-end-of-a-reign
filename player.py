99fe7d2a41e0aba6dc92b0db3c885b9e53ff5fb5
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-20 12:56:15 -0300

    Update player.py

--- player.py
+++ player.py
@@ -36,10 +36,10 @@ class Player:
         self.check_wall_collision(dx, dy)
     
         #
-        if keys[pg.K_LEFT]:
-            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
-        if keys[pg.K_RIGHT]:
-            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        #if keys[pg.K_LEFT]:
+        #    self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        #if keys[pg.K_RIGHT]:
+        #    self.angle += PLAYER_ROT_SPEED * self.game.delta_time
         self.angle %= math.tau
 
     #
@@ -61,8 +61,18 @@ class Player:
         pg.draw.circle(self.game.screen, 'red', (self.x * 100, self.y * 100), 15)
         
     #
+    def mouse_control(self):
+        mx, my = pg.mouse.get_pos()
+        if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+        self.rel = pg.mouse.get_rel()[0]
+        self.rel = max(-MOUSE_MAX_REL, min(MOUSE_MAX_REL, self.rel))
+        self.angle += self.rel * MOUSE_SENSITIVITY * self.game.delta_time
+
+    #
     def update(self):
         self.movement()
+        self.mouse_control()
 
     #
     @property
