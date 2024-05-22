9e80f3fee458ba533af512e0996e1ae84ba172d7
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-22 08:11:26 -0300

    Update sprites.py

---
+++ sprites.py
@@ -0,0 +1,15 @@
+import pygame as pg
+from settings import *
+
+#
+class SpriteObject:
+    def __init__(self, game, path='resources/sprites/static_sprites/ARMRA0.png', pos=(10.5, 3.5)):
+        self.game = game
+        self.player = game.player
+        self.x, self.y = pos
+        self.image = pg.image.load(path).convert_alpha()
+        self.IMAGE_WIDTH = self.image.get_width()
+        self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
+
+    #
+    def get_sprite(self):
