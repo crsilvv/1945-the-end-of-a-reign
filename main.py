969367b4eab5ea05183b6db2aed4259670f8c24e
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-22 14:53:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprites import *
 
 class Game:
     def __init__(self):
@@ -21,11 +22,13 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.static_sprite = SpriteObject(self)
 
     #
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.static_sprite.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
