246bf4540ab0f49f1ec2a2daf763f2ce5afc94aa
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-23 08:47:02 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -2,3 +2,17 @@ from sprites import *
 
 #
 class ObjectHandler:
+    def __init__(self, game):
+        self.game = game
+        self.sprite_list = []
+        self.static_sprite_path = 'resources/sprites/static_sprites/'
+        self.anim_sprite_path = 'resources/sprites/animated_sprites/'
+        add_sprite = self.add_sprite
+
+        #sprite map
+        add_sprite(SpriteObject(game))
+        add_sprite(AnimatedSprite(game))
+    
+    #
+    def add_sprite(self, sprite):
+        self.sprite_list.append(sprite)
