f8c37095dfa3ee2132ca03b2ea8b4deb8a7398c5
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-27 08:24:34 -0300

    Update player.py

--- player.py
+++ player.py
@@ -14,6 +14,7 @@ class Player:
     def single_fire_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1 and not self.shot and not self.game.weapon.reloading:
+                self.game.sound.shotgun.play()
                 self.shot = True
                 self.game.weapon.reloading = True
 
