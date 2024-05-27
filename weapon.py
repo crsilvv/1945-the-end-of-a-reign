3a03ebcf4ea396c684e6aabbd769a65c0d9564a7
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-27 08:12:17 -0300

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -8,7 +8,22 @@ class Weapon(AnimatedSprite):
             [pg.transform.smoothscale(img, (self.image.get_width() * scale, self.image.get_height() * scale))
              for img in self.images])
         self.weapon_pos = (HALF_WIDTH - self.images[0].get_width() // 2, HEIGHT - self.images[0].get_height())
+        self.reloading = False
+        self.num_images = len(self.images)
+        self.frame_counter = 0
+        self.damage = 50
 
+    #
+    def animate_shot(self):
+        if self.reloading:
+            self.game.player.shot = False
+            if self.animation_trigger:
+                self.images.rotate(-1)
+                self.image = self.images[0]
+                self.frame_counter += 1
+                if self.frame_counter == self.num_images:
+                    self.reloading = False
+                    self.frame_counter = 0
 
     #
     def draw(self):
@@ -16,4 +31,5 @@ class Weapon(AnimatedSprite):
 
     #
     def update(self):
-        pass
+        self.check_animation_time()
+        self.animate_shot()
