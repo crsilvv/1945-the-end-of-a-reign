db68e507b772aac0888a5c405c13fb79b02e977e
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-27 08:20:28 -0300

    Create sound.py

---
+++ sound.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+#
+class Sound:
+    def __init__(self, game):
+        self.game = game
+        pg.mixer.init()
+        self.path = 'resources/sounds'
+        self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
