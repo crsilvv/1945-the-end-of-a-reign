5442d2a2db045c639af9bedbe8bb2a9df2812c7f
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-27 08:24:36 -0300

    Update sound.py

--- sound.py
+++ sound.py
@@ -5,5 +5,5 @@ class Sound:
     def __init__(self, game):
         self.game = game
         pg.mixer.init()
-        self.path = 'resources/sounds'
+        self.path = 'resources/sounds/'
         self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
