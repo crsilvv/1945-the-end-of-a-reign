c75282cb81de75c72ec63f07d5362956c0d8620b
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-28 17:04:18 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,13 +1,16 @@
 from sprites import *
+from npc import *
 
 #
 class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
+        self.npc_sprite_path = 'resources/sprites/monsters'
         self.static_sprite_path = 'resources/sprites/static_sprites/'
         self.anim_sprite_path = 'resources/sprites/animated_sprites/'
         add_sprite = self.add_sprite
+        add_npc = self.add_npc
 
         #sprite map
         add_sprite(SpriteObject(game))
@@ -26,8 +29,17 @@ class ObjectHandler:
         add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'heads/0.png', pos=(10.5, 5.5)))
         add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'heads/0.png', pos=(8.5, 7.5)))
 
+        #
+        add_npc(NPC(game))
+
+    #
     def update(self):
         [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
+
+    #
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
     
     #
     def add_sprite(self, sprite):
