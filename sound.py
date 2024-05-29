57a500f898f077882ef2bc1829b0ec54db2a1d93
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-29 10:35:22 -0300

    Update sound.py

--- sound.py
+++ sound.py
@@ -7,3 +7,8 @@ class Sound:
         pg.mixer.init()
         self.path = 'resources/sounds/'
         self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
+        self.monster_pain = pg.mixer.Sound(self.path + 'mosnter_pain.wav')
+        self.monster_death = pg.mixer.Sound(self.path + 'monster_death.wav')
+        self.monster_attack = pg.mixer.Sound(self.path + 'monster_attack.wav')
+        self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
+        self.theme = pg.mixer.music.load(self.path + 'doom_music.mp3')
