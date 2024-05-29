4dae66009d0bfd7b6ab42b97cf9f2b275ec6101b
CR7 <jfmamjjasond_mtwtfss_@outlook.com>
2024-05-29 10:35:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT = 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     #
@@ -42,15 +45,21 @@ class Game:
 
     #
     def draw(self):
+        #self.screen.fill('black')
         self.object_renderer.draw()
         self.weapon.draw()
+        #self.map.draw()
+        #self.player.draw()
 
     #
     def check_events(self):
+        self.global_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
 
     #
